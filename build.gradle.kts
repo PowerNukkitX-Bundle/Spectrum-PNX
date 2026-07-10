5233addb4e205f7d22b1c052b4df23fbcf0a3fea
xRookieFight <me@xrookiefight.com>
2026-07-10 14:20:01 +0300

    feat: switch to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+}
+
+group = "org.powernukkitx"
+version = "1.0-SNAPSHOT"
+5
+repositories {
+    mavenCentral()
+    maven("https://www.jitpack.io")
+    maven("https://repo.powernukkitx.org/releases")
+    maven("https://repo.opencollab.dev/maven-releases")
+    maven("https://repo.opencollab.dev/maven-snapshots")
+}
+
+dependencies {
+    compileOnly(libs.powernukkitx.server)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
